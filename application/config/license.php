a41361eaaf536447a936ec9706f469702257a406
ZK <you@example.com>
2022-01-21 17:24:36 +0500

    Init

---
+++ application/config/license.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SHLK'] = 'WCRCDX-EY926W-PHMBDH-K0NkVjdBQWpoWlV0UjYvYkJpRHRYTUkwOU55eG5UcXRodUdNT1FjSVhrcz0=';
